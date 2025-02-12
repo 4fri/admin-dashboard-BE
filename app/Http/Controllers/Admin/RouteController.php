aa4121c2029921a3848b5b7a2ba4f1d83df69300
Ridho <afridhow78@gmail.com>
2025-02-12 10:58:09 +0700

    - mengubah tipe data name dari tabel route menjadi unik - membuat CRUD untuk menu route

--- app/Http/Controllers/Admin/RouteController.php
+++ app/Http/Controllers/Admin/RouteController.php
@@ -3,20 +3,22 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Permission;
 use App\Models\Route as RouteModel;
 use Illuminate\Http\Request;
 
 class RouteController extends Controller
 {
+    // Get all routes with pagination (10 per page)
     public function index()
     {
         $routes = RouteModel::paginate(10)->through(function ($route) {
             return [
+                "id" => $route->id,
                 "name" => $route->name,
                 "url" => $route->url,
                 "method" => $route->method,
                 "prefix" => $route->prefix,
-                "controller" => $route->controller
             ];
         });
 
@@ -24,5 +26,145 @@ class RouteController extends Controller
             'success' => true,
             'result' => $routes
         ], 200);
+    }
+
+    // Store new route
+    public function store(Request $request)
+    {
+        $validated = $this->validate($request, [
+            'name' => 'required|string|unique:routes,name',
+            'method' => 'required|string',
+            'prefix' => 'nullable|string',
+            'url' => 'required|string',
+            'controller' => 'required|string',
+            'function' => 'required|string',
+        ]);
+
+        $parsData = [
+            'name' => $validated['name'],
+            'method' => strtoupper($validated['method']),
+            'prefix' => $validated['prefix'],
+            'url' => $validated['url'],
+            'controller' => $validated['controller'],
+            'function' => $validated['function'],
+        ];
+
+        try {
+            $route = RouteModel::create($parsData);
+
+            $permission = Permission::create([
+                'name' => $route->name,
+            ]);
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Route created successfully',
+                // 'result' => $routes
+            ], status: 201);
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Route creation failed',
+            ], status: 500);
+        }
+    }
+
+    // Get single route by ID
+    public function show($id)
+    {
+        $route = RouteModel::find($id);
+
+        if (!$route) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Route not found'
+            ], 404);
+        }
+
+        return response()->json([
+            'success' => true,
+            'result' => $route
+        ], 200);
+    }
+
+    // Update route
+    public function update(Request $request, $id)
+    {
+        $route = RouteModel::find($id);
+
+        if (!$route) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Route not found'
+            ], 404);
+        }
+
+        $validated = $this->validate($request, [
+            'name' => 'required|string|unique:routes,name,' . $id,
+            'method' => 'required|string',
+            'prefix' => 'nullable|string',
+            'url' => 'required|string',
+            'controller' => 'required|string',
+            'function' => 'required|string',
+        ]);
+
+        $parsData = [
+            'name' => $validated['name'],
+            'method' => strtoupper($validated['method']),
+            'prefix' => $validated['prefix'],
+            'url' => $validated['url'],
+            'controller' => $validated['controller'],
+            'function' => $validated['function'],
+        ];
+
+        try {
+            $route->update($parsData);
+
+            $permission = Permission::where('name', $validated['name'])->first();
+            $permission->update([
+                'name' => $route->name,
+            ]);
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Route updated successfully',
+                'result' => $route
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Route update failed',
+            ], 500);
+        }
+    }
+
+    // Delete route
+    public function destroy($id)
+    {
+        $route = RouteModel::find($id);
+        $permission = Permission::where('name', $route->name)->first();
+
+        if (!$route) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Route not found'
+            ], 404);
+        }
+
+        try {
+            $permission->roles()->detach();
+            $permission->delete();
+            $route->delete();
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Route deleted successfully'
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Route delete failed',
+            ], 500);
+        }
     }
 }
--- database/migrations/2025_02_10_103958_create_routes_table.php
+++ database/migrations/2025_02_10_103958_create_routes_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('routes', function (Blueprint $table) {
             $table->uuid('id')->primary();
-            $table->string('name');
+            $table->string('name')->unique();
             $table->string('method');
             $table->string('prefix')->nullable();
             $table->string('url');
