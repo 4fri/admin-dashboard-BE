f36633458464d50ff3250fe8e9d2aeaae8678641
Ridho <afridhow78@gmail.com>
2025-02-12 10:13:57 +0700

    membuat app service provider untuk pagination

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,17 @@
 
 namespace App\Providers;
 
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+    public function boot()
+    {
+        Paginator::currentPathResolver(function () {
+            return request()->url();
+        });
+    }
     /**
      * Register any application services.
      *
