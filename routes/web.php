bc65a692fc4f432b991ec6cd1a6fda005a7825a5
Ridho <afridhow78@gmail.com>
2025-02-13 09:58:40 +0700

    fixing route

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,6 @@ use Illuminate\Support\Facades\Schema;
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
-$router->get('/test', 'TestController@index');
 
 $router->group(['prefix' => 'api'], function () use ($router) {
     $router->post('/login', 'AuthController@login');
